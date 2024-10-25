142d3c5bf5ba3054b02620299ee7265f74d896fa
MaximoVolynets <m.volynets@campus.fct.unl.pt>
2024-10-25 18:46:21 +0100

    Cache implementation

---
+++ scc2425-tukano/src/main/java/utils/RedisCache.java
@@ -0,0 +1,38 @@
+package main.java.utils;
+
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
+
+public class RedisCache {
+    private static final String RedisHostname = "scc2425cache70663.redis.cache.windows.net";
+    private static final String RedisKey = "d4NiRsBsCD7M7rMMlMchGdOSCcZIh9Vb9AzCaK571UE=";
+    private static final int REDIS_PORT = 6380;  // Default for Azure
+    private static final int REDIS_TIMEOUT = 1000;
+    private static final boolean Redis_USE_TLS = true;
+
+    private static JedisPool instance;
+
+    public synchronized static JedisPool getCachePool() {
+        if (instance != null)
+            return instance;
+
+        var poolConfig = new JedisPoolConfig();
+        poolConfig.setMaxTotal(128);
+        poolConfig.setMaxIdle(128);
+        poolConfig.setMinIdle(16);
+        poolConfig.setTestOnBorrow(true);
+        poolConfig.setTestOnReturn(true);
+        poolConfig.setTestWhileIdle(true);
+        poolConfig.setNumTestsPerEvictionRun(3);
+        poolConfig.setBlockWhenExhausted(true);
+        try {
+            instance = new JedisPool(poolConfig, RedisHostname, REDIS_PORT, REDIS_TIMEOUT, RedisKey, Redis_USE_TLS);
+        } catch(Exception e) {
+            System.err.println("Error creating Jedis pool: " + e.getMessage());
+            e.printStackTrace();
+            throw new RuntimeException("Unable to create Redis pool", e);  // Re-throw or handle the exception
+        }
+        return instance;
+    }
+}
