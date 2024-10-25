178bebdd2cf35edb7438b14157b12ff0144123d4
MaximoVolynets <m.volynets@campus.fct.unl.pt>
2024-10-25 18:54:41 +0100

    cache implementation done

--- scc2425-tukano/src/main/java/utils/RedisCache.java
+++ scc2425-tukano/src/main/java/utils/RedisCache.java
@@ -5,8 +5,8 @@ import redis.clients.jedis.JedisPool;
 import redis.clients.jedis.JedisPoolConfig;
 
 public class RedisCache {
-    private static final String RedisHostname = "xxxxx.redis.cache.windows.net";
-    private static final String RedisKey = "addKey";
+    private static final String RedisHostname = System.getenv("REDIS_HOSTNAME"); // Change for "redis_hostname
+    private static final String RedisKey = System.getenv("REDIS_KEY"); // Change for the key
     private static final int REDIS_PORT = 6380;  // Default for Azure
     private static final int REDIS_TIMEOUT = 1000;
     private static final boolean Redis_USE_TLS = true;
@@ -34,5 +34,5 @@ public class RedisCache {
             throw new RuntimeException("Unable to create Redis pool", e);  // Re-throw or handle the exception
         }
         return instance;
-    }
+    } 
 }
